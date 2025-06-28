00e85e309c55e1ed746fa757b39621c58a33cb3a
ubninin <144878603+ubninin@users.noreply.github.com>
2025-06-29 08:45:06 +0900

    섹션 1-2강
    -shift 달리기 구현
    -space 점프 구현
    -ctrl 앉기 구현
    -앉은 채로 점프시 앉기 취소 구현

--- 서바이벌 게임/Assets/Scripts/PlayerController.cs
+++ 서바이벌 게임/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,25 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
 {
+    //ㅅ
     [SerializeField] private float walkSpeed;
+    [SerializeField] 
+    private float runSpeed;
+    private float applySpeed;
+    [SerializeField] private float crouchSpeed;
+    [SerializeField] private float jumpForce;
+
+    private bool isRun = false;
+    private bool isGround = true;
+    private bool isCrouch = false;
+
+    //얼마나 앉을지
+    [SerializeField] 
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    private CapsuleCollider capsuleCollider;
     [SerializeField] private float lookSensitivity;
     [SerializeField] 
     private float cameraRotationLimit;
@@ -17,18 +35,107 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
 
         myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
     }
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+        StartCoroutine(CrouchCoroutine());
+    }
+    IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+        while (_posY != applyCrouchPosY)
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if (count > 15)
+            {
+                break;
+            }
+            yield return null;
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY,0f);
+    }
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y+0.1f);
+    }
+    private void TryJump()
+    {
 
+
+        if(Input .GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+    private void Jump()
+    {
+        if (isCrouch)
+        {
+            Crouch();
+        }
+        myRigid.velocity = transform.up * jumpForce;
+    }
+    private void TryRun()
+    {
+        if(Input.GetKey (KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+    private void Running()
+    {
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
     private void Move()
     {
         float _moveDirX = Input.GetAxisRaw("Horizontal");
@@ -36,7 +143,7 @@ public class PlayerController : MonoBehaviour
 
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirZ;
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized *applySpeed;
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
 
     }
