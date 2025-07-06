4d93126a88359ea1cb4c8c78f34f00d68846448e
ubninin <144878603+ubninin@users.noreply.github.com>
2025-07-06 16:43:41 +0900

    섹션 2-8강
    - ui 추가

--- 서바이벌 게임/Assets/Scripts/GunController.cs
+++ 서바이벌 게임/Assets/Scripts/GunController.cs
@@ -212,4 +212,8 @@ public class GunController : MonoBehaviour
         audioSource.clip = _clip;
         audioSource.Play();
     }
+    public Gun GetGun()
+    {
+        return currentGun;
+    }
 }
