598d059459820ace368e2d92673474dc1bc9aed3
ubninin <144878603+ubninin@users.noreply.github.com>
2025-07-09 06:52:50 +0900

    섹션 3-2강
    - 사운드 매니저 추가

--- 서바이벌 게임/Assets/Scripts/Rock.cs
+++ 서바이벌 게임/Assets/Scripts/Rock.cs
@@ -5,38 +5,42 @@ using UnityEngine;
 public class Rock : MonoBehaviour
 {
 
-    [SerializeField]private int hp; // 바위체력
-    [SerializeField]private float destroyTime; // 파편 제거 시간
+    [SerializeField]private int hp; 
+
+    [SerializeField]private float destroyTime; 
+
     [SerializeField]private SphereCollider col; 
 
-    //오브젝트
-    [SerializeField]private GameObject go_rock; // 일반 바위
-    [SerializeField]private GameObject go_debris; // 깨진 바위
-    [SerializeField]private GameObject go_effect_prefabs; // 채굴 이펙트
+    [SerializeField]
+    private GameObject go_rock; // 일반 바위
+    [SerializeField]
+    private GameObject go_debris; // 깨진 바위
+    [SerializeField]
+    private GameObject go_effect_prefabs; // 채굴 이펙트
 
-
-    [SerializeField]private AudioSource audioSource;
-    [SerializeField]private AudioClip effect_sound;
-    [SerializeField]private AudioClip effect_sound2;
+    //사운드 이름
+    [SerializeField]private string strike_Sound;
+    [SerializeField]private string destroy_Sound;
 
 
     public void Mining()
     {
-        audioSource.clip = effect_sound;
-        audioSource.Play();
+        SoundManager.instance.PlaySE(strike_Sound);
+
         var clone = Instantiate(go_effect_prefabs, col.bounds.center, Quaternion.identity);
         Destroy(clone, destroyTime);
         hp--;
+
         if (hp <= 0)
             Destruction();
     }
 
     private void Destruction()
     {
-        audioSource.clip = effect_sound2;
-        audioSource.Play();
+        SoundManager.instance.PlaySE(destroy_Sound);
         col.enabled = false;
         Destroy(go_rock);
+
         go_debris.SetActive(true);
         Destroy(go_debris, destroyTime);
     }
